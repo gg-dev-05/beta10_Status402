d239ef10fa9f11b1c5ff0c47ea1772a04e7673c5
Kushaan Gowda <kushaangowda@yahoo.com>
2021-10-30 19:14:51 +0530

    created a route to check type of user

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,25 @@
 const router = require("express").Router();
+const jwt = require("jsonwebtoken");
+
+require("dotenv").config();
 
 router.route("/").get((req, res) => {
 	res.status(200).json({ statusCode: 200, message: "Hello World" });
+});
+
+router.route("/checkUser").post((req, res) => {
+	const token = req.body.token;
+	try {
+		jwt.verify(token, process.env.PRIVATE_KEY_FARMERS);
+		res.status(200).json({ statusCode: 200, user: "farmer" });
+	} catch (err) {
+		try {
+			jwt.verify(token, process.env.PRIVATE_KEY_CONSUMERS);
+			res.status(200).json({ statusCode: 200, user: "consumer" });
+		} catch (err1) {
+			res.status(200).json({ statusCode: 200, user: "undefined" });
+		}
+	}
 });
 
 module.exports = router;
