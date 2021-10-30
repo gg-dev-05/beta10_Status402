1f9f70d85ec15b9bb3a39084a1f91b7dfea3185e
Kushaan Gowda <kushaangowda@yahoo.com>
2021-10-30 19:07:56 +0530

    finished signup and login

--- frontend/src/views/examples/Register.js
+++ frontend/src/views/examples/Register.js
@@ -16,150 +16,141 @@
 
 */
 
+import React, { useState } from "react";
+
 // reactstrap components
 import {
-  Button,
-  Card,
-  CardHeader,
-  CardBody,
-  FormGroup,
-  Form,
-  Input,
-  InputGroupAddon,
-  InputGroupText,
-  InputGroup,
-  Row,
-  Col,
+	Button,
+	Card,
+	CardHeader,
+	CardBody,
+	FormGroup,
+	Form,
+	Input,
+	InputGroupAddon,
+	InputGroupText,
+	InputGroup,
+	Row,
+	Col,
 } from "reactstrap";
 
+import "../../assets/css/register.css";
+import { BASE_URL } from "../../Common/Constants";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+
 const Register = () => {
-  return (
-    <>
-      <Col lg="6" md="8">
-        <Card className="bg-secondary shadow border-0">
-          {/* <CardHeader className="bg-transparent pb-5">
-            <div className="text-muted text-center mt-2 mb-4">
-              <small>Sign up with</small>
-            </div>
-            <div className="text-center">
-              <Button
-                className="btn-neutral btn-icon mr-4"
-                color="default"
-                href="#pablo"
-                onClick={(e) => e.preventDefault()}
-              >
-                <span className="btn-inner--icon">
-                  <img
-                    alt="..."
-                    src={
-                      require("../../assets/img/icons/common/github.svg")
-                        .default
-                    }
-                  />
-                </span>
-                <span className="btn-inner--text">Github</span>
-              </Button>
-              <Button
-                className="btn-neutral btn-icon"
-                color="default"
-                href="#pablo"
-                onClick={(e) => e.preventDefault()}
-              >
-                <span className="btn-inner--icon">
-                  <img
-                    alt="..."
-                    src={
-                      require("../../assets/img/icons/common/google.svg")
-                        .default
-                    }
-                  />
-                </span>
-                <span className="btn-inner--text">Google</span>
-              </Button>
-            </div>
-          </CardHeader> */}
-          <CardBody className="px-lg-5 py-lg-5">
-            <div className="text-center text-muted mb-4">
-              <small>Sign Up</small>
-            </div>
-            <Form role="form">
-              <FormGroup>
-                <InputGroup className="input-group-alternative mb-3">
-                  <InputGroupAddon addonType="prepend">
-                    <InputGroupText>
-                      <i className="ni ni-hat-3" />
-                    </InputGroupText>
-                  </InputGroupAddon>
-                  <Input placeholder="Name" type="text" />
-                </InputGroup>
-              </FormGroup>
-              <FormGroup>
-                <InputGroup className="input-group-alternative mb-3">
-                  <InputGroupAddon addonType="prepend">
-                    <InputGroupText>
-                      <i className="ni ni-email-83" />
-                    </InputGroupText>
-                  </InputGroupAddon>
-                  <Input
-                    placeholder="Email"
-                    type="email"
-                    autoComplete="new-email"
-                  />
-                </InputGroup>
-              </FormGroup>
-              <FormGroup>
-                <InputGroup className="input-group-alternative">
-                  <InputGroupAddon addonType="prepend">
-                    <InputGroupText>
-                      <i className="ni ni-lock-circle-open" />
-                    </InputGroupText>
-                  </InputGroupAddon>
-                  <Input
-                    placeholder="Password"
-                    type="password"
-                    autoComplete="new-password"
-                  />
-                </InputGroup>
-              </FormGroup>
-              <div className="text-muted font-italic">
-                <small>
-                  password strength:{" "}
-                  <span className="text-success font-weight-700">strong</span>
-                </small>
-              </div>
-              <Row className="my-4">
-                <Col xs="12">
-                  <div className="custom-control custom-control-alternative custom-checkbox">
-                    <input
-                      className="custom-control-input"
-                      id="customCheckRegister"
-                      type="checkbox"
-                    />
-                    <label
-                      className="custom-control-label"
-                      htmlFor="customCheckRegister"
-                    >
-                      <span className="text-muted">
-                        I agree with the{" "}
-                        <a href="#pablo" onClick={(e) => e.preventDefault()}>
-                          Privacy Policy
-                        </a>
-                      </span>
-                    </label>
-                  </div>
-                </Col>
-              </Row>
-              <div className="text-center">
-                <Button className="mt-4" color="primary" type="button">
-                  Create account
-                </Button>
-              </div>
-            </Form>
-          </CardBody>
-        </Card>
-      </Col>
-    </>
-  );
+	let history = useHistory();
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+
+	const [userType, setUserType] = useState(0);
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		const body = {
+			username,
+			password,
+		};
+
+		let url = BASE_URL;
+
+		if (userType == 0) url += "farmer/add";
+		else url += "consumer/add";
+
+		const res = await axios.post(url, body);
+		if (res.data.statusCode == 200) {
+			history.push("/auth/login");
+		} else {
+			// cannot add user
+		}
+	};
+
+	return (
+		<>
+			<Col lg="6" md="8">
+				<Card className="bg-secondary shadow border-0">
+					<CardBody className="px-lg-5 py-lg-5">
+						<div className="text-center text-muted mb-4">
+							<small>Sign Up</small>
+						</div>
+						<Form
+							role="form"
+							method="POST"
+							onSubmit={(e) => {
+								handleSubmit(e);
+							}}
+						>
+							<FormGroup>
+								<InputGroup className="input-group-alternative mb-3">
+									<InputGroupAddon addonType="prepend">
+										<InputGroupText>
+											<i className="ni ni-email-83" />
+										</InputGroupText>
+									</InputGroupAddon>
+									<Input
+										placeholder="Username"
+										type="text"
+										autoComplete="new-email"
+										onChange={(e) => {
+											setUsername(e.target.value);
+										}}
+									/>
+								</InputGroup>
+							</FormGroup>
+							<FormGroup>
+								<InputGroup className="input-group-alternative">
+									<InputGroupAddon addonType="prepend">
+										<InputGroupText>
+											<i className="ni ni-lock-circle-open" />
+										</InputGroupText>
+									</InputGroupAddon>
+									<Input
+										placeholder="Password"
+										type="password"
+										autoComplete="new-password"
+										onChange={(e) => {
+											setPassword(e.target.value);
+										}}
+									/>
+								</InputGroup>
+							</FormGroup>
+							<div
+								className="d-flex align-items-center"
+								onClick={() => {
+									setUserType(0);
+								}}
+								style={{ cursor: "pointer" }}
+							>
+								<div className={userType === 0 ? "blueBox" : "whiteBox"}></div>
+								<label htmlFor="customCheckRegister" style={{ cursor: "pointer", marginBottom: 0 }}>
+									<span>Farmer</span>
+								</label>
+							</div>
+							<div
+								className="d-flex align-items-center mb-4"
+								onClick={() => {
+									setUserType(1);
+								}}
+								style={{ cursor: "pointer" }}
+							>
+								<div className={userType === 1 ? "blueBox" : "whiteBox"}></div>
+								<label htmlFor="customCheckRegister" style={{ cursor: "pointer", marginBottom: 0 }}>
+									<span>Consumer</span>
+								</label>
+							</div>
+							<div className="text-center">
+								<Button className="mt-4" color="primary" type="submit">
+									Create account
+								</Button>
+							</div>
+						</Form>
+					</CardBody>
+				</Card>
+			</Col>
+		</>
+	);
 };
 
 export default Register;
