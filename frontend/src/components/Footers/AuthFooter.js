427fd7b02690c913cc2c2098c173798a588a3087
Kushaan Gowda <kushaangowda@yahoo.com>
2021-10-31 10:35:30 +0530

    changed footer

--- frontend/src/components/Footers/AuthFooter.js
+++ frontend/src/components/Footers/AuthFooter.js
@@ -21,52 +21,35 @@
 import { NavItem, NavLink, Nav, Container, Row, Col } from "reactstrap";
 
 const Login = () => {
-  return (
-    <>
-      <footer className="py-5">
-        <Container>
-          <Row className="align-items-center justify-content-xl-between">
-            <Col xl="6">
-              <div className="copyright text-center text-xl-left text-muted">
-                © {new Date().getFullYear()}{" "}
-                <a className="font-weight-bold ml-1" href="#">
-                  Farmer Helper
-                </a>
-              </div>
-            </Col>
-            <Col xl="6">
-              <Nav className="nav-footer justify-content-center justify-content-xl-end">
-                <NavItem>
-                  <NavLink href="#">Farmer Helper</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink href="https://www.creative-tim.com/presentation?ref=adr-auth-footer">
-                    About Us
-                  </NavLink>
-                </NavItem>
-                {/* <NavItem>
-                  <NavLink
-                    href="http://blog.creative-tim.com?ref=adr-auth-footer"
-                    target="_blank"
-                  >
-                    Blog
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink
-                    href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md?ref=adr-auth-footer"
-                    target="_blank"
-                  >
-                    MIT License
-                  </NavLink>
-                </NavItem> */}
-              </Nav>
-            </Col>
-          </Row>
-        </Container>
-      </footer>
-    </>
-  );
+	return (
+		<>
+			<footer className="py-5">
+				<Container>
+					<Row className="align-items-center justify-content-xl-between">
+						<Col xl="6">
+							<div className="copyright text-center text-xl-left text-muted">
+								© {new Date().getFullYear()}{" "}
+								<span className="font-weight-bold ml-1">Farmer Helper</span>
+							</div>
+						</Col>
+						<Col xl="6">
+							<Nav className="nav-footer justify-content-center justify-content-xl-end">
+								<NavItem>
+									<NavLink
+										href="https://github.com/gg-dev-05/beta10_Status402"
+										rel="noopener noreferrer"
+										target="_blank"
+									>
+										Created by Status402
+									</NavLink>
+								</NavItem>
+							</Nav>
+						</Col>
+					</Row>
+				</Container>
+			</footer>
+		</>
+	);
 };
 
 export default Login;
